e9e13fcfd30430fec3ccd5f1f9a3a69676784dfe
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-05-08 21:12:03 +0530

    Realtime Device Tracking System

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", {
+      id: socket.id,
+      ...data,
+    });
+  });
+  console.log("Connected !");
+
+  socket.on("disconnect", () => {
+      io.emit("user-disconnect", {
+          id: socket.id,
+        });
+        console.log("user disconnected !");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port http://localhost:3000");
+});
