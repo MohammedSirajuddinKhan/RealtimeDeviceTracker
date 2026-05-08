12502ae7a0d1d08e564120afd269af42c02b1b77
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-05-08 21:13:16 +0530

    minor changes

--- public/js/script.js
+++ public/js/script.js
@@ -20,7 +20,7 @@ if (navigator.geolocation) {
 const map = L.map("map").setView([0, 0], 18);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "OpenStreetMap",
+  attribution: "Sirajuddin Khan",
 }).addTo(map);
 
 const markers = {};
